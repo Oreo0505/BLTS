6cc2bb837e0315aa28fc9dff83e92138877ac889
DailyLollipops <clarencemadrigal84@gmail.com>
2023-05-17 10:11:49 +0800

    Fixed typo

--- database/migrations/2023_05_11_045420_create_authors_table.php
+++ database/migrations/2023_05_11_045420_create_authors_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('position');
-            $table->foreignId('term');
+            $table->foreignId('term_id');
             $table->timestamps();
         });
     }
