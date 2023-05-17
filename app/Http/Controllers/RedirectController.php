91cf2c232cb5c7760592b7c2be2a7ae91b8f716c
DailyLollipops <clarencemadrigal84@gmail.com>
2023-05-17 10:23:50 +0800

    Added redirect to setup on first use

--- app/Http/Controllers/RedirectController.php
+++ app/Http/Controllers/RedirectController.php
@@ -5,10 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Author;
 use App\Models\Document;
+use App\Models\Config;
 
 class RedirectController extends Controller
 {
     public function redirectToHomepage(){
+        $config = Config::first();
+        if($config->first_time){
+            return redirect('/setup');
+        }
+
         $documents = Document::with('authors')->get();
         return view('welcome',[
             'documents' => $documents
