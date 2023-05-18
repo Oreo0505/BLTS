70941a906ae5bf70871970377ab391f9740fe4a0
DailyLollipops <clarencemadrigal84@gmail.com>
2023-05-18 10:47:14 +0800

    Changed year dropdown

--- resources/views/components/modal/filter.blade.php
+++ resources/views/components/modal/filter.blade.php
@@ -19,16 +19,15 @@
         </div>
         <div class="flex flex-col items-center relative w-2/3 md:w-1/3">
             <select name="filter-year" id="filter-year" class="type peer h-full w-full rounded-[7px] border border-gray-700 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-gray-700 placeholder-shown:border-t-gray-700 empty:!bg-red-500 focus:border-2 focus:border-gray-700 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-gray-700">
-                <option value="all" class="text-sm">All Year</option>
-                <option value="2018" class="text-sm">2018</option>
-                <option value="2019" class="text-sm">2019</option>
-                <option value="2020" class="text-sm">2020</option>
-                <option value="2021" class="text-sm">2021</option>
-                <option value="2022" class="text-sm">2022</option>
-                <option value="2023" class="text-sm">2023</option>
+                <option value="all" class="text-sm">All Terms</option>
+                <option value="Older" class="text-sm">Older Terms</option>
+                <option value="2012-2015" class="text-sm">2012-2015</option>
+                <option value="2015-2018" class="text-sm">2015-2018</option>
+                <option value="2018-2021" class="text-sm">2018-2021</option>
+                <option value="2021-2024" class="text-sm">2021-2024</option>
             </select>
             <label for="filter-year" class="before:content[\' \'] after:content[\' \'] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-gray-700 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-gray-700 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-gray-700 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-gray-700 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-700 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-gray-700 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-gray-700 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-gray-700">
-                Year
+                Administrative Year
             </label>
         </div>
         <div class="flex flex-col items-center relative w-2/3 md:w-1/3">
