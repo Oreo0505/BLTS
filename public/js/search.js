ca7e2dab5d32bb94cc62d235170306ed90fd293a
DailyLollipops <clarencemadrigal84@gmail.com>
2023-05-21 11:18:19 +0800

    Added search value length constraint

--- public/js/search.js
+++ public/js/search.js
@@ -2,6 +2,10 @@ const searchForm = document.getElementById('search-form');
 const searchField = document.getElementById('search-field');
 
 searchField.addEventListener('keydown', function(event){
+    if(searchField.value.length < 3){
+        alert('Please enter at least 3 characters');
+        return;
+    }
     if(event.key == 'Enter'){
         searchForm.submit();
     }
