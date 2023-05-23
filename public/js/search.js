940360ec1493b3dc5b7942f808c6861f2a890092
DailyLollipops <clarencemadrigal84@gmail.com>
2023-05-23 08:09:45 +0800

    Fixed search bug

--- public/js/search.js
+++ public/js/search.js
@@ -3,10 +3,13 @@ const searchField = document.getElementById('search-field');
 
 searchField.addEventListener('keydown', function(event){
     if(event.key == 'Enter'){
+        event.preventDefault();
         if(searchField.value.length < 3){
             alert('Please enter at least 3 characters');
             return;
         }
-        searchForm.submit();
+        else{
+            searchForm.submit();
+        }
     }
 })
