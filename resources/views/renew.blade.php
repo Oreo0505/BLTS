090630f4e9f9136e401c4ccc89404cea5d45af35
DailyLollipops <clarencemadrigal84@gmail.com>
2023-05-25 08:16:34 +0800

    Fixed typo

--- resources/views/renew.blade.php
+++ resources/views/renew.blade.php
@@ -105,7 +105,7 @@
                             <input type="text" id="sb7" name="sb7" value="{{ old('sb7') }}" placeholder="SB Member No.7" class="sb-member w-full mt-1 border-0 border-gray-700 font-sans font-normal px-1 py-0 text-sm text-gray-700 placeholder:text-xs">
                         </div>
                         <div class="h-2 flex flex-row space-x-2 items-center">
-                            <label for="chairman" class="font-sans font-normal text-sm text-gray-700 leading-tight mt-1">7.</label>
+                            <label for="chairman" class="font-sans font-normal text-sm text-gray-700 leading-tight mt-1">8.</label>
                             <input type="text" id="chairman" name="chairman" value="{{ old('chairman') }}" placeholder="SK Chairman" class="w-full mt-1 border-0 border-gray-700 font-sans font-normal px-1 py-0 text-sm text-gray-700 placeholder:text-xs">
                         </div>
                     </div>
