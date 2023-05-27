85aa36319898a4d0f908b3730aba8f3b08c9647e
DailyLollipops <clarencemadrigal84@gmail.com>
2023-05-27 10:42:26 +0800

    Added filter search function

--- public/js/search.js
+++ public/js/search.js
@@ -1,8 +1,14 @@
 const searchForm = document.getElementById('search-form');
 const searchField = document.getElementById('search-field');
+const searchFilterButton = document.getElementById('search-filter');
 
 searchField.addEventListener('keydown', function(event){
     if(event.key == 'Enter'){
         searchForm.submit();
     }
 })
+
+searchFilterButton.addEventListener('click', function(){
+    searchField.value = '';
+    searchForm.submit();
+});
