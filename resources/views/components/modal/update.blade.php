8e0751c70c25332b83ff9307c855964772b3f333
DailyLollipops <clarencemadrigal84@gmail.com>
2023-05-27 09:49:03 +0800

    Fixed button message and attributes

--- resources/views/components/modal/update.blade.php
+++ resources/views/components/modal/update.blade.php
@@ -104,8 +104,8 @@
         </div> 
     </form>
     <div class="flex flex-row justify-end self-end space-x-2 mx-6 px-2 pb-3">
-        <button id="update-cancel" class="px-2 py-1.5 font-sans font-medium text-sm text-[#1976D2] hover:text-gray-400">SEARCH</button>
-        <button id="update-submit" class="px-2 py-1.5 font-sans font-medium text-sm text-[#1976D2] hover:text-gray-400">APPLY</button>
+        <button id="update-cancel" type="button" class="px-2 py-1.5 font-sans font-medium text-sm text-[#1976D2] hover:text-gray-400">CANCEL</button>
+        <button id="update-submit" type="button" class="px-2 py-1.5 font-sans font-medium text-sm text-[#1976D2] hover:text-gray-400">UPDATE</button>
     </div>
 </div>
 
--- resources/views/components/modal/upload.blade.php
+++ resources/views/components/modal/upload.blade.php
@@ -103,8 +103,8 @@
         </div> 
     </form>
     <div class="flex flex-row justify-end self-end space-x-2 mx-6 px-2 pb-3">
-        <button id="upload-cancel" class="px-2 py-1.5 font-sans font-medium text-sm text-[#1976D2] hover:text-gray-400">SEARCH</button>
-        <button id="upload-submit" class="px-2 py-1.5 font-sans font-medium text-sm text-[#1976D2] hover:text-gray-400">APPLY</button>
+        <button id="upload-cancel" type="button" class="px-2 py-1.5 font-sans font-medium text-sm text-[#1976D2] hover:text-gray-400">CANCEL</button>
+        <button id="upload-submit" type="button" class="px-2 py-1.5 font-sans font-medium text-sm text-[#1976D2] hover:text-gray-400">UPLOAD</button>
     </div>
 </div>
 
