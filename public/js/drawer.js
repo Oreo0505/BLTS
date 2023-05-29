ecc75516cbba748c2471ca304c4a64b9bac9aad1
DailyLollipops <clarencemadrigal84@gmail.com>
2023-05-29 10:51:06 +0800

    Logo update feature moved to update profile page

--- public/js/drawer.js
+++ public/js/drawer.js
@@ -2,9 +2,6 @@ const drawer = document.getElementById('drawer');
 const drawerOverlay = document.getElementById('drawer-overlay');
 const openDrawerButton = document.getElementById('open-drawer');
 const closeDrawerButton = document.getElementById('close-drawer');
-const logoHolder = document.getElementById('logo-holder');
-const updateLogoForm = document.getElementById('update-logo-form');
-const updateLogoField = document.getElementById('update-logo-file');
 
 function openDrawer(){
     drawerOverlay.classList.remove('hidden');
@@ -21,11 +18,3 @@ openDrawerButton.addEventListener('click', openDrawer);
 closeDrawerButton.addEventListener('click', closeDrawer);
 
 drawerOverlay.addEventListener('click', closeDrawer);
-
-logoHolder.addEventListener('click', function(){
-    updateLogoField.click();
-})
-
-updateLogoField.addEventListener('change', function(){
-    updateLogoForm.submit();
-});
