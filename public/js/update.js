43f20b3dfdfcb585dbd1afe77d782fb79afe5425
DailyLollipops <clarencemadrigal84@gmail.com>
2023-06-05 16:10:24 +0800

    Fixed author dropdown not displaying current selected authors count

--- public/js/update.js
+++ public/js/update.js
@@ -258,7 +258,7 @@ function changeUpdateAuthorSelection(data){
                     selected++;
                 }
             }
-            uploadAuthorDropdown.innerHTML = `Select authors (${selected} out of 9 selected)
+            updateAuthorDropdown.innerHTML = `Select authors (${selected} out of 9 selected)
                 <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
                 </svg>`
