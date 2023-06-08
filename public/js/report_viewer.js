e8fef7a0563465b9b6d514b0753124382719b02f
DailyLollipops <clarencemadrigal84@gmail.com>
2023-06-08 15:51:04 +0800

    Fixed variable typos

--- public/js/report_viewer.js
+++ public/js/report_viewer.js
@@ -6,10 +6,10 @@ const reportViewer = document.getElementById('report-viewer');
 reportButton.addEventListener('click', function(){
     reportViewerFrame.src = '/storage/Reports/report.pdf';
     reportViewer.classList.remove('hidden');
-    closeViewerButton.classList.remove('hidden');
+    closeReportViewerButton.classList.remove('hidden');
 });
 
 closeReportViewerButton.addEventListener('click', function(){
     reportViewer.classList.add('hidden');
-    closeViewerButton.classList.add('hidden');
+    closeReportViewerButton.classList.add('hidden');
 })
