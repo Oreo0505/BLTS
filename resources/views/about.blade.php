d78a95a37e95296f368c9a2666f7f73be908096e
DailyLollipops <clarencemadrigal84@gmail.com>
2023-06-08 16:12:23 +0800

    Added manual

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -25,7 +25,7 @@
                     <br>stored, such as Code of Ordinance, Ordinance, Resolution, Proposal, and others which are proposed and enacted by Barangays,
                     <br>are distinguished with which administrative term they were uploaded.
                 </p>
-                <a href="#" class="flex bg-white rounded-full mt-9 px-6 py-3 font-sans font-bold text-lg text-[#425B71] text-center">
+                <a href="/manual.pdf" target="_norefer" class="flex bg-white rounded-full mt-9 px-6 py-3 font-sans font-bold text-lg text-[#425B71] text-center">
                     View Documentation
                 </a>
                 <hr id="breakpoint">
